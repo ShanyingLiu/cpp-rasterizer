0944104af90be0c4f6347ebb0654135f50e3b4ca
Shanying Liu <jl200850984@gmail.com>
2026-06-21 16:02:46 -0400

    Improved camera coordinate transforms

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <tuple>
 #include "geometry.h"
 #include "model.h"
+#include <algorithm>
 #include "tgaimage.h"
 
-constexpr int width  = 128;
-constexpr int height = 128;
+// GLOBAL DECLARATIONS 
+constexpr int width  = 512;
+constexpr int height = 512;
+
+mat<4,4> ModelView, Viewport, Perspective;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // bGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -13,6 +17,7 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+// DRAW A LINE
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
     bool steep = std::abs(ax-bx) < std::abs(ay-by);
     if (steep) { // if the line is steep, we transpose the image
@@ -38,11 +43,13 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+// DRAW A TRIANGLE
+
 double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
 }
 
-void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz,  TGAImage &zbuffer, TGAImage &framebuffer, TGAColor color) {
+void triangle(int ax, int ay, double az, int bx, int by, double bz, int cx, int cy, double cz,  std::vector<double> &zbuffer, TGAImage &framebuffer, TGAColor color) {
     int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
     int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
     int bbmaxx = std::max(std::max(ax, bx), cx);
@@ -57,14 +64,15 @@ void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, in
             double beta  = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha<0 || beta<0 || gamma<0) continue; // negative barycentric coordinate => the pixel is outside the triangle
-            unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
-            if (z <= zbuffer.get(x, y)[0]) continue; // if current z is behind last buffer drawn we skip
-            zbuffer.set(x, y, {z}); // z converted to color
+            double z = alpha * az + beta * bz + gamma * cz; // z as a double, mapped from 2d to 1d
+            if (z <= zbuffer[x+y*width]) continue; // if current z is behind last buffer drawn we skip
+            zbuffer[x+y*width] = z;
             framebuffer.set(x, y, color);
         }
     }
 }
 
+// OLD CAMERA CONFIG
 
 vec3 rot(vec3 v) {
     constexpr double a = M_PI/6;
@@ -84,26 +92,76 @@ std::tuple<int,int,int> project(vec3 v) { // First of all, (x,y) is an orthogona
 }
 
 
+// SETUP GLOBAL VLAUES
+void viewport(const int x, const int y, const int w, const int h) {
+    Viewport = {{{w/2., 0, 0, x+w/2.}, {0, h/2., 0, y+h/2.}, {0,0,1,0}, {0,0,0,1}}};
+}
+
+void perspective(const double f) {
+    Perspective = {{{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0, -1/f,1}}};
+}
+
+void lookat(const vec3 eye, const vec3 center, const vec3 up) {
+    vec3 n = normalized(eye-center);
+    vec3 l = normalized(cross(up,n));
+    vec3 m = normalized(cross(n, l));
+    ModelView = mat<4,4>{{{l.x,l.y,l.z,0}, {m.x,m.y,m.z,0}, {n.x,n.y,n.z,0}, {0,0,0,1}}} *
+                mat<4,4>{{{1,0,0,-center.x}, {0,1,0,-center.y}, {0,0,1,-center.z}, {0,0,0,1}}};
+}
+
+void rasterize(const vec4 clip[3], std::vector<double> &zbuffer, TGAImage &framebuffer, const TGAColor color) {
+    vec4 ndc[3]    = { clip[0]/clip[0].w, clip[1]/clip[1].w, clip[2]/clip[2].w };                // normalized device coordinates
+    vec2 screen[3] = { (Viewport*ndc[0]).xy(), (Viewport*ndc[1]).xy(), (Viewport*ndc[2]).xy() }; // screen coordinates
+
+    mat<3,3> ABC = {{ {screen[0].x, screen[0].y, 1.}, {screen[1].x, screen[1].y, 1.}, {screen[2].x, screen[2].y, 1.} }};
+    if (ABC.det()<1) return; // backface culling + discarding triangles that cover less than a pixel
+
+    auto [bbminx,bbmaxx] = std::minmax({screen[0].x, screen[1].x, screen[2].x}); // bounding box for the triangle
+    auto [bbminy,bbmaxy] = std::minmax({screen[0].y, screen[1].y, screen[2].y}); // defined by its top left and bottom right corners
+#pragma omp parallel for
+    for (int x=std::max<int>(bbminx, 0); x<=std::min<int>(bbmaxx, framebuffer.width()-1); x++) { // clip the bounding box by the screen
+        for (int y=std::max<int>(bbminy, 0); y<=std::min<int>(bbmaxy, framebuffer.height()-1); y++) {
+            vec3 bc = ABC.invert_transpose() * vec3{static_cast<double>(x), static_cast<double>(y), 1.}; // barycentric coordinates of {x,y} w.r.t the triangle
+            if (bc.x<0 || bc.y<0 || bc.z<0) continue;                                                    // negative barycentric coordinate => the pixel is outside the triangle
+            double z = bc * vec3{ ndc[0].z, ndc[1].z, ndc[2].z };
+            if (z <= zbuffer[x+y*framebuffer.width()]) continue;
+            zbuffer[x+y*framebuffer.width()] = z;
+            framebuffer.set(x, y, color);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
         return 1;
     }
 
-    Model model(argv[1]);
-    TGAImage framebuffer(width, height, TGAImage::RGB);
-    TGAImage     zbuffer(width, height, TGAImage::GRAYSCALE);
+    constexpr vec3    eye{-1,0,2}; // camera position
+    constexpr vec3 center{0,0,0};  // camera direction
+    constexpr vec3     up{0,1,0};  // camera up vector
 
-    for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
-        auto [ax, ay, az] = project(rot(model.vert(i, 0)));
-        auto [bx, by, bz] = project(rot(model.vert(i, 1)));
-        auto [cx, cy, cz] = project(rot(model.vert(i, 2)));
-        TGAColor rnd;
-        for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-        triangle(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer, rnd);
+    lookat(eye, center, up);                              // build the ModelView   matrix
+    perspective(norm(eye-center));                        // build the Perspective matrix
+    viewport(width/16, height/16, width*7/8, height*7/8); // build the Viewport    matrix
+
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+    std::vector<double> zbuffer(width*height, -std::numeric_limits<double>::max());
+
+    for (int m=1; m<argc; m++) { // iterate through all input objects
+        Model model(argv[m]);
+        for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
+            vec4 clip[3];
+            for (int d : {0,1,2}) {            // assemble the primitive
+                vec3 v = model.vert(i, d);
+                clip[d] = Perspective * ModelView * vec4{v.x, v.y, v.z, 1.};
+            }
+            TGAColor rnd;
+            for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
+            rasterize(clip, zbuffer, framebuffer, rnd); // rasterize the primitive
+        }
     }
 
     framebuffer.write_tga_file("framebuffer.tga");
-    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
